32580fa9d28d78c3762e1e7a78ede4b1302ef185
hurli <hurli@DESKTOP-7K5HDPQ>
2023-11-09 19:34:00 +0100

    Add project files.

---
+++ HotelDatabase/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection cnn;
+            connectionString = @"Data Source=localhost;Initial Catalog=master;Integrated Security=True";
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+            SqlCommand command;
+            SqlDataReader dataReader;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            string sql, Output = "";
+
+            sql = "Select Hotel_No, Name, Address from DemoHotel";
+
+            command = new SqlCommand(sql, cnn);
+            adapter.InsertCommand = new SqlCommand(sql, cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + dataReader.GetValue(2) + "\n";
+            }
+            dataReader.Close();
+            command.Dispose();
+            cnn.Close();
+        }
+
+    }
+}
