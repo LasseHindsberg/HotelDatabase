4091217c603336438a7561bd6331a9873c4faae4
LasseHindsberg <lassehindsberg@gmail.com>
2023-11-15 19:48:32 +0100

    added Query for adding new Table

---
+++ HotelDatabase/Queries/Query.sql
@@ -0,0 +1,11 @@
+﻿CREATE DATABASE TrainingDatabase
+
+USE TrainingDatabase
+
+CREATE TABLE Facilities(Facility_No int NOT NULL PRIMARY KEY, Name VARCHAR(30) NOT NULL)
+INSERT INTO Facilities VALUES (1, 'Pool')
+INSERT INTO Facilities VALUES (2, 'Billiard')
+INSERT INTO Facilities VALUES (3, 'Biograf')
+INSERT INTO Facilities VALUES (4, 'Bordtennis')
+INSERT INTO Facilities VALUES (5, 'Bar')
+INSERT INTO Facilities VALUES (6, 'Bordfodbold')
