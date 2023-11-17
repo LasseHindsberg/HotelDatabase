5ada073aefb2352fa85c8eb66dd8fb5a3975bec9
LasseHindsberg <lassehindsberg@gmail.com>
2023-11-17 03:18:09 +0100

    Added CRUD ability across application

--- HotelDatabase/Form1.cs
+++ HotelDatabase/Form1.cs
@@ -8,46 +8,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace HotelDatabase
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=TrainingDatabase;Integrated Security=True");
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+        }
+
+        private void cleardata()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+        private void displaydata()
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select Facility_No, Name from dbo.Facilities";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection cnn;
-            connectionString = @"Data Source=localhost;Initial Catalog=TrainingDatabase;Integrated Security=True";
-            cnn = new SqlConnection(connectionString);
+            displaydata();
+        }
 
-            cnn.Open();
-            MessageBox.Show("Connection Open !");
-            SqlCommand command;
-            SqlDataReader dataReader;
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            string sql, Output = "";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Clears previous Parameters(Text box) incase it/Others has been run before
+            cmd.Parameters.Clear();
 
-            sql = "Select Facility_No, Name from dbo.Facilities";
+            string query = "INSERT INTO Facilities VALUES (@FacilityID, @FacilityName)";
+            conn.Open();
+            // For some reason this stuff has Swapped so textbox 2 = id and not name.
+            cmd.Parameters.AddWithValue("@FacilityID", textBox2.Text);
+            cmd.Parameters.AddWithValue("@FacilityName", textBox1.Text);
 
-            command = new SqlCommand(sql, cnn);
-            adapter.InsertCommand = new SqlCommand(sql, cnn);
-            adapter.InsertCommand.ExecuteNonQuery();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            cleardata();
+            conn.Close();
+            displaydata();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Clears previous Parameters(Text box) incase it/Others has been run before
+            cmd.Parameters.Clear();
 
-            dataReader = command.ExecuteReader();
+            conn.Open();
+            cmd.CommandType = CommandType.Text;
+            string query = "UPDATE Facilities SET Name=(@FacilityName) WHERE Facility_No=(@FacilityID)";
+            cmd.Parameters.AddWithValue("@FacilityID", textBox2.Text);
+            cmd.Parameters.AddWithValue("@FacilityName", textBox1.Text);
 
-            while (dataReader.Read())
-            {
-                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
-            }
-            MessageBox.Show(Output);
-            dataReader.Close();
-            command.Dispose();
-            cnn.Close();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            displaydata();
+            cleardata();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Clears previous Parameters(Text box) incase it/Others has been run before
+            cmd.Parameters.Clear();
+
+
+            string query = "DELETE Facilities WHERE Facility_No=(@FacilityID)";
+            cmd.Parameters.AddWithValue("@FacilityID", textBox2.Text);
+
+            cmd.CommandText = query;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            dataGridView1.DataSource = query;
+            cleardata();
+            conn.Close();
+            displaydata();
         }
 
     }
